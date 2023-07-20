6fa69c5e1c20b2fb37f01aacf9c549d0d2fc97ab
Maria Sokolenko <maria23@pluralcamp.com>
2023-07-20 12:46:37 +0200

    falso

--- script.js
+++ script.js
@@ -5,11 +5,7 @@
 //     "total_pages": 2,
 //     "data": [
 //         {
-//             "id": 1,
-//             "email": "george.bluth@reqres.in",
-//             "first_name": "George",
-//             "last_name": "Bluth",
-//             "avatar": "https://reqres.in/img/faces/1-image.jpg"
+//            
 //         },
 //         {
 //             "id": 2,
